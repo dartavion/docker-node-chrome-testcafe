c5329476aa5117aefb8bad2bd3ed2c98f7baf08d
Richard Ranke <richard_ranke@homedepot.com>
2019-08-06 21:26:45 -0400

    delete

--- features/step_definitions/markdown.js
+++
@@ -1,6 +0,0 @@
-const {Given, When, Then} = require('cucumber');
-const Selector = require('testcafe').Selector;
-
-Given('I have opened Order Up', async function () {
-    await testController.navigateTo('https://usom-client-accepted.apps-np.homedepot.com');
-});
